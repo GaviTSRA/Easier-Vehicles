c54bb52f279aeddcea91cc7f78e81321cbbba237
GaviTSRA <gavitsra@gmail.com>
2023-10-23 11:56:42 +0200

    auto. insert ammo, only send message to one player

--- control.lua
+++ control.lua
@@ -33,6 +33,14 @@ script.on_event(defines.events.on_player_driving_changed_state, function(e)
    end
 end)
 
+function on_enter(e)
+   local player = game.players[e.player_index]
+   if e.entity.get_driver() ~= nil and e.entity.get_driver().player == player then
+      fuel_vehicle(e, e.entity)
+      ammo_vehicle(e, e.entity)
+   end
+end
+
 function fuel_vehicle(e, entity)
    if entity.get_fuel_inventory() == nil then
       return
@@ -56,18 +64,49 @@ function fuel_vehicle(e, entity)
    for i, v in pairs(fuels) do
       if player.get_item_count(v.name) > 0 then
          amount = math.min(player.get_item_count(v.name), v.stack_size)
-         game.print({"", "Inserted " .. amount .. " ", {"item-name." .. v.name}})
+         player.print({"", "Inserted " .. amount .. " ", {"item-name." .. v.name}})
          entity.insert{name=v.name, count=amount}
          player.remove_item{name=v.name, count=amount}
          return
       end
    end
-   game.print("No fuel to insert!")
+   player.print("No fuel to insert!")
 end
 
-function on_enter(e)
-   local player = game.players[e.player_index]
-   if e.entity.get_driver() ~= nil and e.entity.get_driver().player == player then
-      fuel_vehicle(e, e.entity)
+function ammo_vehicle(e, entity)
+   local inv = nil
+   if entity.get_inventory(defines.inventory.car_ammo) ~= nil then
+      inv = entity.get_inventory(defines.inventory.car_ammo)
    end
+   if entity.get_inventory(defines.inventory.spider_ammo) ~= nil then
+      inv = entity.get_inventory(defines.inventory.spider_ammo)
+   end
+   if inv == nil then
+      return
+   end
+
+   if not inv.is_empty() then
+      return
+   end
+
+   local player = game.players[e.player_index]
+
+   local ammo = {}
+   for k,v in pairs(game.item_prototypes) do
+      if v.get_ammo_type() ~= nil and inv.can_insert(k) then
+         table.insert(ammo, v)
+      end
+   end
+
+   for i, v in pairs(ammo) do
+      if player.get_item_count(v.name) > 0 then
+         amount = math.min(player.get_item_count(v.name), v.stack_size)
+         player.print({"", "Inserted " .. amount .. " ", {"item-name." .. v.name}})
+         entity.insert{name=v.name, count=amount}
+         player.remove_item{name=v.name, count=amount}
+         return
+      end
+   end
+   player.print("No ammo to insert!")
 end
+
